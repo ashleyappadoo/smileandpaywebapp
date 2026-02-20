2a5a5e2d408e61192f73d169ecccb516bf92f1b6
ashleyappadoo <125403028+ashleyappadoo@users.noreply.github.com>
2026-02-20 16:12:46 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 import os
 
-API_URL = "https://europe-west1-smileandpay-1d455.cloudfunctions.net/paymentWe-V1_PROD"
+API_URL = "https://europe-west1-smileandpay-1d455.cloudfunctions.net/paymentWeb-V1_PROD"
 
 st.set_page_config(layout="wide")
 
